4dcddc1fc008c102127e4b5a5f59478747af4cdb
Luiz Felipe Moureau <luizfelipesmoureau@gmail.com>
2026-06-01 01:28:57 +0000

    Configure website package deployment and update bun.lock

---
+++ packages/website/deploy.ts
@@ -0,0 +1,28 @@
+import { Basebox } from "@moureau/basebox";
+
+const { BASEBOX_ANON_KEY, BASEBOX_SECRET_KEY } = process.env;
+
+const dist = "./dist/ssg";
+const domain = "murow.moureau.dev";
+const publicKey = BASEBOX_ANON_KEY;
+const apiKey = BASEBOX_SECRET_KEY;
+
+const bb = new Basebox({ publicKey });
+
+const main = async () => {
+    console.time("Deployment time");
+
+    const { success } = await bb
+      .managed({ apiKey })
+      .deploy({ dist, domain });
+
+    if (!success) {
+      console.log("Failed to deploy.");
+      return;
+    }
+
+    console.log(`Deployed successfully at https://${domain}`);
+    console.timeEnd("Deployment time");
+}
+
+main();
